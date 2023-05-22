ef77ea687ceb1698d2f6c95a082092eff7406420
alfandi <fandidixpc1@gmail.com>
2023-05-22 18:46:33 +0700

    menambah procedur pasien untuk menambah dokter

--- Asosiasi/Asosiasi.cpp
+++ Asosiasi/Asosiasi.cpp
@@ -36,4 +36,8 @@ public:
 void pasien::tambahDokter(dokter* pDokter) {
 	daftar_dokter.push_back(pDokter);
 }
-void pasien
+void pasien::cetakDokter() {
+	cout << "Daftar Dokter yang menangani pasien \"" << this->nama << "\":\n";
+	for (auto& a : daftar_dokter) {
+		cout << a->nama << "\n";
+	}
