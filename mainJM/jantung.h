ab830989a066a5a21da04bd8912fbb5c00628652
alfandi <fandidixpc1@gmail.com>
2023-05-22 19:03:41 +0700

    header jantung tidak ikut ke push

--- mainJM/jantung.h
+++ mainJM/jantung.h
@@ -1,1 +1,14 @@
 #pragma once
+#ifndef JANTUNG_H
+#define JANTUNG_H
+
+class jantung {
+public:
+	jantung() {
+		cout << "Jantung  dihidupkan";
+	}
+	~jantung() {
+		cout << "jantung dimatikan\n";
+	}
+};
+#endif // !JANTUNG_H
